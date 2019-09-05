27bc345731364f4c69346c370e1cdc839448d718
yourmybee <nadianur111@gmail.com>
2019-09-05 18:14:05 +0700

    push latihan 1 hari ke 4 (OOP)

---
+++ minggu-1/04/latihan-1/Point.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author NADIA
+ */
+public class Point {
+
+    public int x = 0;
+    public int y = 0;
+    // a constructor!
+    public Point(int a, int b) {
+    x = a;
+    y = b;
+    }
+}   
